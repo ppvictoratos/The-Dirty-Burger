10843b1e259979c46508b185a2bc3421259f88ce
petiepositivo <pvictoratos@rocketmail.com>
2023-01-09 17:58:08 -0500

    add sui project
    added the version that i started to make in SUI

--- OneTwoThreeMath/OneTwoThreeMath/OneTwoThreeMathApp.swift
+++ OneTwoThreeMath/OneTwoThreeMath/OneTwoThreeMathApp.swift
@@ -2,6 +2,8 @@
 //  OneTwoThreeMathApp.swift
 //  OneTwoThreeMath
 //
+//  Note that we have the OG and SUI Project folders, that has all of our old code. We're going to have to pick through it before we can get this running. I'll comment out any blockers for now but we should heavily reference what we already have.
+//
 //  Created by Petie Positivo on 1/9/23.
 //
 
